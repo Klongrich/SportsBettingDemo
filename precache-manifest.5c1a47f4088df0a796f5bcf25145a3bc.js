2f436134457687aa4bf7c52c0602c36e8525ae37
Kyle Longrich <longrichk@gmail.com>
2020-09-10 09:03:52 -0400

    Updates

---
+++ precache-manifest.5c1a47f4088df0a796f5bcf25145a3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9609383c95e94adadf477a9c53560e06",
+    "url": "/SportsBettingDemo/index.html"
+  },
+  {
+    "revision": "227cd8faa6e9a544b3c5",
+    "url": "/SportsBettingDemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "637c0cee366d80827df8",
+    "url": "/SportsBettingDemo/static/js/2.20140a71.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/SportsBettingDemo/static/js/2.20140a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "227cd8faa6e9a544b3c5",
+    "url": "/SportsBettingDemo/static/js/main.aff774eb.chunk.js"
+  },
+  {
+    "revision": "829c570b286171dbc729",
+    "url": "/SportsBettingDemo/static/js/runtime-main.97c27446.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SportsBettingDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
