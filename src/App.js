398cf3ae8c0ab87d8981a721b0a4317905fe12d2
Kyle Longrich <longrichk@gmail.com>
2020-09-10 08:46:42 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import HomePage from './HomePage/index'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomePage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomePage/index.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import styled from "styled-components";
+
+
+const Container = styled.div`
+
+    border: 2px solid white;
+    border-radius: 10px;
+
+    ul{
+        list-style-type: none;
+        float: left;
+
+        margin-right: 40px;
+        margin-top: 8px;
+    }
+
+    li {
+        padding-top: 20px;
+    }
+`
+
+const BetContainer = styled.div`
+
+    border: 2px solid white;
+    border-radius: 5px;
+
+    opacity: 0.5;
+
+    background-color: grey;
+
+    text-align: center;
+
+    margin-top: 10px;
+
+    li {
+        margin-top: -10px;
+        padding-bottom: 10px;
+    }
+
+    &:hover {
+        color: white;
+        opacity: 1;
+    }
+
+`
+
+const BetButton = styled.div`
+
+    border: 2px solid white;
+    border-radius: 5px;
+
+    opacity: 0.5;
+
+    height: 20px;
+    width: 180px;
+
+    display: inline-block;
+    background-color: green;
+
+    text-align: center;
+
+    margin-right: 20px;
+
+    margin-top: 123px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+
+    &:hover {
+        color: white;
+        opacity: 1;
+    }
+
+`
+
+export default function Homepage() {
+
+    return (
+        <>
+        <Container>
+            <ul>
+                <li>Browns vs Stealers</li>
+                <li>Team One:</li>
+                <li>Team Two:</li>
+            </ul>
+
+            <ul>
+                <li>Money Line </li>
+
+                <BetContainer>
+                    <li> +130</li>
+                </BetContainer>
+
+                <BetContainer>
+                    <li> -120 </li>
+                </BetContainer>
+            </ul>
+
+            <ul>
+                <li>Over / Under </li>
+
+                <BetContainer>
+                    <li>+7 +140</li>
+                </BetContainer>
+
+                <BetContainer>
+                    <li>-7 -120</li>
+                </BetContainer>
+            </ul>
+
+            <BetButton>
+                Bet
+            </BetButton>
+        </Container>
+        </>
+    )
+
+}
